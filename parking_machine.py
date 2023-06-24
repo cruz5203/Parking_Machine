7d9255111cb382ceea93303930917641306b5d85
cruz5203 <91767967+cruz5203@users.noreply.github.com>
2023-06-24 21:23:20 +0800

    V2.0

--- parking_machine.py
+++ parking_machine.py
@@ -1,45 +1,279 @@
-from tkinter import Tk, Label, Button, Entry, Text, ttk
+from tkinter import Tk, ttk, Label, Entry, Button, Text, Toplevel
+import tkinter.messagebox as mbox
+import parking_machine_import
 
-window = Tk()  # 創建主視窗
-window.minsize(width=500, height=300)  # 設置視窗最小大小
-window.title("停車繳費系統")  # 設置視窗標題
-window.resizable(True, True)  # 設置視窗是否可以調整大小
-a = ttk.Frame(window, padding=10)
-# window.grid()# 創建網格佈局
-window.grid_rowconfigure(0, minsize=100)  # 設置標籤
 
-label = Label(window, text="輸入車牌", padx=10, pady=20)
+car_infos = {}
+money = 0
+car_name = None
 
-# 設置標籤位置
-label.grid(row=0, column=0)
-# label.place(x=50, y=50)
 
-# 設置文字輸入框
-entry = Entry(window, width=30)
+def error_money():
+    mbox.showwarning("警告", "非法貨幣")
 
-# 設置文字輸入框位置
-entry.grid(row=0, column=1)
 
-# 設置按鈕
-button = Button(window, text="確定")
+def pay_money():
+    if car_name == None:
+        mbox.showwarning("警告", "請輸入車牌")
+    else:
+        if car_infos[car_name].seach_car_pay() <= money:
+            ctl = Toplevel(window)
+            ctl.title("付款成功")
+            ctl.geometry("100x170")
+            ctl.resizable(False, False)
+            car_name_lable = Label(ctl, text=car_name, justify="center")
+            car_name_lable.grid(row=0, column=0, padx=5, pady=5)
+            car_in = Label(
+                ctl,
+                text="應付金額:"
+                + str(
+                    car_infos[car_name].seach_car_pay(),
+                ),
+                justify="center",
+            )
+            car_in.grid(row=1, column=0, padx=5, pady=5)
+            car_out = Label(ctl, text="實付金額:" + str(money), justify="center")
+            car_out.grid(row=2, column=0, padx=5, pady=5)
+            car_more = Label(
+                ctl,
+                text="找零:" + str(money - car_infos[car_name].seach_car_pay()),
+                justify="center",
+            )
 
-# 設置按鈕位置
-button.grid(row=0, column=2)
+            car_more.grid(row=3, column=0, padx=5, pady=5)
 
-# 設置文本
-text = Text(window, width=40, height=5)
+            def yes():
+                ctl.destroy()
 
-# 設置文本位置
-text.grid(row=1, column=1, columnspan=2, pady=10)
-label1 = Label(window, text="金額", padx=10, pady=20)
-label1.grid(row=2, column=0)
-entry1 = Entry(window, width=10)
-entry1.grid(row=2, column=1, columnspan=2, sticky="W")
+            btn = Button(ctl, text="確認", width=5, command=yes)
+            btn.grid(row=4, column=0, padx=5, pady=5)
+            car_infos.pop(car_name)
+        else:
+            mbox.showerror("付款失敗", "投入金額不足")
+        cancel_pay_money()
 
-button1 = Button(window, text="確定付款")
-button1.grid(row=3, column=0)
-button2 = Button(window, text="取消付款")
-button2.grid(row=3, column=1)
 
-# 執行視窗循環
+def cancel_pay_money():
+    sreach_answer_text.config("")
+
+
+def sreach_car_name():
+    global car_name
+    if car_name_entry.get() in car_infos:
+        car_name = car_name_entry.get()
+        car_infos[car_name].car_out()
+        sreach_answer_text.config(state="normal")
+        sreach_answer_text.insert("1.0", "\n")
+        sreach_answer_text.insert("2.0", car_name + "\n\n")
+        sreach_answer_text.insert(
+            "4.0",
+            "進場:" + car_infos[car_name].search_car_in() + "\n\n",
+        )
+        sreach_answer_text.insert(
+            "6.0",
+            "出場:" + car_infos[car_name].search_car_out() + "\n\n",
+        )
+        sreach_answer_text.insert(
+            "8.0",
+            "費用:" + str(car_infos[car_name].seach_car_pay()) + "\n\n",
+        )
+        sreach_answer_text.tag_configure("center", justify="center")
+        sreach_answer_text.tag_add("center", "1.0", "end")
+        sreach_answer_text.config(state="disabled")
+    elif car_name_entry.get() == "":
+        mbox.showwarning("警告", "請輸入車牌")
+    else:
+        mbox.showwarning("警告", "查無車牌\n請重新確認車牌號碼")
+        global money
+        money = 0
+        car_name_entry.delete(0, "end")
+        sreach_answer_text.config(state="normal")
+        sreach_answer_text.delete("1.0", "end")
+        sreach_answer_text.config(state="disabled")
+        new_money()
+
+
+def convert_to_uppercase():
+    input_text = car_name_entry.get()
+    uppercase_text = input_text.upper()
+    car_name_entry.delete(0, "end")
+    car_name_entry.insert(0, uppercase_text)
+
+
+def new_car_name():
+    if in_car_name_entry.get() in car_infos:
+        mbox.showwarning("警告", "車牌已存在\n請在確認車牌")
+    else:
+        car = parking_machine_import.ParkingMachine()
+        car.add_car(in_car_name_entry.get())
+        car_infos[in_car_name_entry.get()] = car
+
+
+def cancel_pay_money():
+    global need_money
+    global car_name
+    global money
+    global car_in_time
+    global car_out_time
+    need_money = 0
+    car_name = ""
+    money = 0
+    car_in_time = 0.0
+    car_out_time = 0.0
+    car_name_entry.delete(0, "end")
+    sreach_answer_text.config(state="normal")
+    sreach_answer_text.delete("1.0", "end")
+    sreach_answer_text.config(state="disabled")
+    new_money()
+
+
+def new_money():
+    enter_money_entry.config(state="normal")
+    enter_money_entry.delete(0, "end")
+    enter_money_entry.insert(0, str(money))
+    enter_money_entry.config(state="readonly")
+
+
+def money_1():
+    global money
+    money += 1
+    new_money()
+
+
+def money_5():
+    global money
+    money += 5
+    new_money()
+
+
+def money_10():
+    global money
+    money += 10
+    new_money()
+
+
+def money_50():
+    global money
+    money += 50
+    new_money()
+
+
+def money_100():
+    global money
+    money += 100
+    new_money()
+
+
+def money_200():
+    global money
+    money += 200
+    new_money()
+
+
+def money_500():
+    global money
+    money += 500
+    new_money()
+
+
+def money_1000():
+    global money
+    money += 1000
+    new_money()
+
+
+def money_2000():
+    global money
+    money += 2000
+    new_money()
+
+
+window = Tk()
+window.geometry("370x270")
+window.resizable(False, False)
+window.title("停車繳費系統")
+
+frame = ttk.Frame(window, padding=10)
+frame.pack()
+
+car_name_lable = Label(frame, text="車牌號碼:")
+car_name_lable.grid(row=0, column=0, padx=5, pady=5)
+
+car_name_entry = Entry(frame, width=30)
+car_name_entry.grid(row=0, column=1, padx=5, pady=5)
+# car_name_entry.config(validate="key", validatecommand=convert_to_uppercase)
+
+sreach_btn = Button(frame, text="查詢", command=sreach_car_name)
+sreach_btn.grid(row=0, column=2, padx=5, pady=5)
+
+sreach_answer_text = Text(frame, width=45, height=10, state="disabled")
+sreach_answer_text.grid(row=1, column=0, columnspan=3, padx=5, pady=5)
+
+enter_money_lable = Label(frame, text="投入金額:")
+enter_money_lable.grid(row=2, column=0, padx=5, pady=5)
+
+enter_money_entry = Entry(frame, width=10, justify="center")
+enter_money_entry.grid(row=2, column=1, padx=5, pady=5, sticky="w")
+enter_money_entry.insert(0, str(money))
+enter_money_entry.config(state="readonly")
+
+frame_1 = ttk.Frame(frame)
+frame_1.grid(row=3, column=0, columnspan=3, sticky="nsew")
+
+yes_btn = Button(frame_1, text="確認付款", width=20, command=pay_money)
+yes_btn.grid(row=0, column=0, padx=10, pady=5)
+
+no_btn = Button(frame_1, text="取消付款", width=20, command=cancel_pay_money)
+no_btn.grid(row=0, column=1, padx=10, pady=5)
+
+ctl = Toplevel(window)
+ctl.title("控制台")
+ctl.geometry("370x130")
+ctl.resizable(False, False)
+
+ctl_frame = ttk.Frame(ctl, padding=10)
+ctl_frame.pack()
+
+in_car_name_lable = Label(ctl_frame, text="車牌號碼:")
+in_car_name_lable.grid(row=0, column=0, padx=5, pady=5)
+
+in_car_name_entry = Entry(ctl_frame, width=30)
+in_car_name_entry.grid(row=0, column=1, padx=5, pady=5)
+
+car_in_btn = Button(ctl_frame, text="入場", command=new_car_name)
+car_in_btn.grid(row=0, column=2, padx=5, pady=5)
+
+ctl_frame_money = ttk.Frame(ctl_frame)
+ctl_frame_money.grid(row=1, column=0, columnspan=3)
+
+money_btn_1 = Button(ctl_frame_money, text="1", width=7, command=money_1)
+money_btn_1.grid(row=0, column=0, padx=6, pady=5)
+
+money_btn_5 = Button(ctl_frame_money, text="5", width=7, command=money_5)
+money_btn_5.grid(row=1, column=0, padx=6, pady=5)
+
+money_btn_10 = Button(ctl_frame_money, text="10", width=7, command=money_10)
+money_btn_10.grid(row=0, column=1, padx=6, pady=5)
+
+money_btn_50 = Button(ctl_frame_money, text="50", width=7, command=money_50)
+money_btn_50.grid(row=1, column=1, padx=6, pady=5)
+
+money_btn_100 = Button(ctl_frame_money, text="100", width=7, command=money_100)
+money_btn_100.grid(row=0, column=2, padx=6, pady=5)
+
+money_btn_200 = Button(ctl_frame_money, text="200", width=7, command=money_200)
+money_btn_200.grid(row=1, column=2, padx=6, pady=5)
+
+money_btn_500 = Button(ctl_frame_money, text="500", width=7, command=money_500)
+money_btn_500.grid(row=0, column=3, padx=6, pady=5)
+
+money_btn_1000 = Button(ctl_frame_money, text="1000", width=7, command=money_1000)
+money_btn_1000.grid(row=1, column=3, padx=6, pady=5)
+
+money_btn_2000 = Button(ctl_frame_money, text="2000", width=7, command=money_2000)
+money_btn_2000.grid(row=0, column=4, padx=6, pady=5)
+
+money_btn_error = Button(ctl_frame_money, text="-1", width=7, command=error_money)
+money_btn_error.grid(row=1, column=4, padx=6, pady=5)
+
 window.mainloop()
--- parking_machine_import.py
+++ parking_machine_import.py
@@ -1,86 +1,59 @@
 import datetime
+import math
 
-parking_data = {}
 
-
-class Parkingpayment:
+class ParkingMachine:
     def __init__(self):
-        self.funcion_obj = funcion()  # 创建funcion类的实例
+        self.license_plate = None
+        self.car_in = None
+        self.car_outs = None
+        self.cost = None
 
-    def add_car(self, license_plate):
-        parking_time = datetime.datetime.now()
-        data = {"parking_time_in": parking_time}
-        car_data.record_parking_data(license_plate, data)
+    def add_car(self, license_plate):  # 加入車輛
+        car_in = datetime.datetime.now()
+        self.car_in = car_in
+        self.license_plate = license_plate
 
-    def search_car_in(self, license_plate):
-        parking_time = car_data.get_parking_data(license_plate).get("parking_time_in")
-        formatted_time = parking_time.strftime("%Y-%m-%d %H:%M:%S")
-        print("車牌", license_plate, "的停車時間：", formatted_time)
-        return parking_time
+    def search_car(self, license_plate):  # 查詢車輛是否存在
+        if self.license_plate != None:
+            return True
+        else:
+            return False
 
-    def search_car_out(self, license_plate):
+    def car_out(self):  # 離場
         parking_time_out = datetime.datetime.now()
         parking_time_out += datetime.timedelta(minutes=3000)
-        car_data.add_additional_data(
-            license_plate, "parking_time_out", parking_time_out
-        )
-        parking_time = car_data.get_parking_data(license_plate).get("parking_time_out")
-        formatted_time = parking_time.strftime("%Y-%m-%d %H:%M:%S")
-        print("車牌", license_plate, "的離場時間：", formatted_time)
-        return parking_time
+        self.car_outs = parking_time_out
 
-    def seach_car_pay(self, license_plate):
-        car_in = car_data.get_parking_data(license_plate).get("parking_time_in")
-        car_out = car_data.get_parking_data(license_plate).get("parking_time_out")
-        minutes_parked = self.funcion_obj.time(car_in, car_out)
-        day = car_in.weekday() + 1
-        money = self.funcion_obj.money(
-            license_plate, minutes_parked, day
-        )  # 调用money方法计算价格
+    def time1(self, time):  # 可控離場時間
+        car_out = self.car_in
+        car_out += datetime.timedelta(minutes=time)
+        self.car_outs = car_out
 
+    def search_car_in(self):  # 查詢入場時間
+        car_in = self.car_in
+        formatted_time = car_in.strftime("%Y-%m-%d %H:%M:%S")
+        return formatted_time.split(".")[0]  # 移除秒的小數部分
+
+    def search_car_out(self):  # 查詢出場時間
+        car_outs = self.car_outs
+        formatted_time = car_outs.strftime("%Y-%m-%d %H:%M:%S")
+        return formatted_time.split(".")[0]  # 移除秒的小數部分
+
+    def seach_car_pay(self):  # 查詢價格
+        if self.car_in is None or self.car_outs is None:
+            print("車牌", self.license_plate, "的入場或離場時間未完整記錄")
+            return 0
+        minutes_parked = self.time()
+        day = self.car_in.weekday() + 1
+        money = self.money(minutes_parked, day)
         return money
 
-    def pay_money(self, license_plate):
-        return 0
-
-    def give_change(self, license_plate):
-        return 0
-
-    def delete(self, license_plate):
-        return 0
-
-
-class car_data:
-    @staticmethod
-    def record_parking_data(license_plate, data):
-        if license_plate not in parking_data:
-            parking_data[license_plate] = {}
-        parking_data[license_plate].update(data)
-
-    @staticmethod
-    def get_parking_data(license_plate):
-        if license_plate in parking_data:
-            return parking_data[license_plate]
-        else:
-            return None
-
-    @staticmethod
-    def add_additional_data(license_plate, key, value):
-        if license_plate in parking_data:
-            parking_data[license_plate][key] = value
-
-
-class funcion:
-    def time(self, car_in, car_out):
-        parking_time = car_out - car_in
-        minutes_parked = parking_time.total_seconds() // 60
-        return minutes_parked
-
-    def money(self, license_plate, minutes_parked, day):  # 價錢判斷
-        moneys = 0
-        car_in = car_data.get_parking_data(license_plate).get("parking_time_in")
-        car_in_data = car_in.minute
+    def money(self, minutes_parked, day):
+        car_date = self.car_outs - self.car_in
+        car_in_data = self.car_in.minute
         minutes_parkeds = 0
+        moneys = 0  # 宣告和初始化
         while minutes_parked > 0:
             if day > 7:
                 day = 1
@@ -90,13 +63,14 @@ class funcion:
             else:
                 rate = 15
                 max_charge = 300
-            if car_in.hour != 0 or car_in.minute != 0:
+            if car_date.days != 0 and self.car_in.hour != 0 and self.car_in.minute != 0:
                 minutes_parkeds = 1440 - car_in_data
                 minutes_parked -= minutes_parkeds
                 day += 1
             else:
                 if minutes_parked < 1440:
                     minutes_parkeds = minutes_parked
+                    minutes_parked -= minutes_parkeds
                 else:
                     minutes_parkeds = 1440
                     minutes_parked -= minutes_parkeds
@@ -106,29 +80,94 @@ class funcion:
             moneys += payment
         return moneys
 
+    def pay_money(self, payment, money):  # 支付判斷
+        if money is None:
+            print("找不到價格資訊")
+            return False
+        if payment >= money:
+            return True
+        else:
+            return False
+
+    def give_change(self, payment, money):  # 找零
+        if money is None:
+            print("找不到價格資訊")
+            return None
+        givechange = payment - money
+        return givechange
+
+    def time(self):  # 分鐘計算
+        parking_time = self.car_outs - self.car_in
+        minutes_parked = parking_time.total_seconds() // 60
+        return minutes_parked
+
     def is_weekend(self, day):  # 判斷假日
         return day in [6, 7]
 
-    def make_payment(self, amount):  # 支付系統
-        coins = []
-        while amount > 0:
-            coin = int(input("請投入硬幣（5、10, 50, 100, 500, 1000）: "))
-            if coin in [5, 10, 50, 100, 200, 500, 1000, 2000]:
-                coins.append(coin)
-                amount -= coin
+
+# 主程式測試功能
+if __name__ == "__main__":
+    # 創建一個停車場物件
+    parking_machine = ParkingMachine()
+
+    while True:
+        print("請選擇操作：")
+        print("1. 加入車輛")
+        print("2. 查詢車輛是否存在")
+        print("3. 車輛離場")
+        print("4. 查詢車輛入場時間")
+        print("5. 查詢車輛離場時間")
+        print("6. 查詢車輛應支付金額")
+        print("7. 進行支付")
+        print("8. 離開系統")
+
+        choice = input("請輸入選項：")
+
+        if choice == "1":
+            license_plate = input("請輸入車牌號碼：")
+            parking_machine.add_car(license_plate)
+            print("車輛已加入")
+
+        elif choice == "2":
+            license_plate = input("請輸入車牌號碼：")
+            car_exists = parking_machine.search_car(license_plate)
+            if car_exists:
+                print("車輛存在")
             else:
-                print("不支援該硬幣")
+                print("車輛不存在")
 
+        elif choice == "3":
+            license_plate = input("請輸入車牌號碼：")
+            parking_machine.car_out(license_plate)
+            print("車輛已離場")
 
-payment = Parkingpayment()
+        elif choice == "4":
+            license_plate = input("請輸入車牌號碼：")
+            car_in_time = parking_machine.search_car_in(license_plate)
+            print("車輛入場時間：", car_in_time)
 
-# 新增車輛的停車資料
-payment.add_car("ABC123")
+        elif choice == "5":
+            license_plate = input("請輸入車牌號碼：")
+            car_out_time = parking_machine.search_car_out(license_plate)
+            print("車輛離場時間：", car_out_time)
 
-# 查詢車輛的停車時間
-parking_time_ABC123 = payment.search_car_in("ABC123")
-parking_time_ABC123 = payment.search_car_out("ABC123")
+        elif choice == "6":
+            license_plate = input("請輸入車牌號碼：")
+            money = parking_machine.seach_car_pay(license_plate)
+            print("需要支付的金額：", money)
 
-# 获取停车时间并计算费用
-minutes_parked = payment.seach_car_pay("ABC123")
-print("金錢", minutes_parked)
+        elif choice == "7":
+            payment = float(input("請輸入支付金額："))
+            money = parking_machine.seach_car_pay(license_plate)
+            if parking_machine.pay_money(payment, money):
+                change = parking_machine.give_change(payment, money)
+                print("支付成功，找零：", change)
+            else:
+                print("支付金額不足")
+
+        elif choice == "8":
+            print("感謝使用，再見！")
+            break
+
+        else:
+            print("請輸入有效的選項")
